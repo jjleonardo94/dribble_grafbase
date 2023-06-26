cc3aeba9ea0312fa9a844bdb34871077145ea507
Juan Jose Leonardo Amparo <jjleonardoamparo@gmail.com>
2023-06-26 12:15:12 -0400

    first commit

---
+++ components/AuthProviders.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthProviders = () => {
+  return (
+    <div>
+      <div>AuthProviders</div>
+    </div>
+  );
+};
+
+export default AuthProviders;
---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,32 @@
+import { g, auth, config } from "@grafbase/sdk";
+
+const User = g.model("User", {
+  name: g.string().length({ min: 2, max: 100 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(),
+  projects: g.relation(),
+});
+// .auth((rules) => {
+//   rules.public().read()
+// })
+
+const Project = g.model("Project", {
+  title: g.string().length({ min: 3 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+});
+// .auth((rules) => {
+//   rules.public().read()
+//   rules.private().create().delete().update()
+// })
+
+export default config({
+  schema: g,
+});
